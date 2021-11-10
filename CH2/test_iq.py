653208390d53bca96bf6a4d1bec09b2771bc7b2e
Unknown-92 <syakirin61299@gmail.com>
2021-11-10 19:05:12 +0800

    Chapter_2

---
+++ CH2/test_iq.py
@@ -0,0 +1,12 @@
+percent = float(input("What is your percentage? "))
+
+if 90 <= percent <100:
+	print("You received an A")
+elif 80 <= percent <90:
+	print("You received a B")
+elif 70 <= percent <80:
+	print("You received a C")
+elif 60 <= percent <70:
+	print("You received a D")
+else:
+	print("oops, not good")
